2960d935ad52010e13bf5e6f8c9ec61639f6ef44
MKRadius <caomanhkhai110@gmail.com>
2024-12-05 05:56:10 +0200

    Add css for the index page

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This is the start.sh script for Linux/Unix
+
+# Run the Python script
+python3 main.py
---
+++ start_windows.bat
@@ -0,0 +1,5 @@
+@echo off
+:: This is the start_windows.bat script for Windows
+
+:: Run the Python script
+python main.py
